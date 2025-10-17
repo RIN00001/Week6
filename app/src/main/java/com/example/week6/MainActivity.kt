927a8fd4a87fabdd4977ca808bdce4359cedbe8b
RIN00001 <hamerboy37@gmail.com>
2025-10-17 22:42:49 +0700

    Copyright Infrigment

---
+++ app/src/main/java/com/example/week6/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.week6
+//you are my fire
+//the one desire
+//believe when I say
+//I want it that way
+//tell me why
+//ain't nothin' but a heartache
+//tell me why
+//ain't nothin' but a mistake
+//now number 5
+//i never wanna hear you say
+//i want it that way
