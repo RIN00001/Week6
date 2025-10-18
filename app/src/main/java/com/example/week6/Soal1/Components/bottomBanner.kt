82a10a59bc19bb8d5e8df432474abc23d924ae10
RIN00001 <hamerboy37@gmail.com>
2025-10-18 08:23:42 +0700

    Soal 1 Import Cleanup

--- app/src/main/java/com/example/week6/Soal1/Components/bottomBanner.kt
+++ app/src/main/java/com/example/week6/Soal1/Components/bottomBanner.kt
@@ -16,9 +16,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.week6.R
+
+@Preview
+@Composable
+fun BottomBannerPreview() {
+    BottomBanner()
+}
+
 
 @Composable
 fun BottomBanner() {
--- app/src/main/java/com/example/week6/Soal1/UIs/Veggie.kt
+++ app/src/main/java/com/example/week6/Soal1/UIs/Veggie.kt
@@ -1,25 +1,19 @@
 package com.example.week6.Soal1.UIs
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.week6.R
 import com.example.week6.Soal1.Components.AppTitle
+import com.example.week6.Soal1.Components.BottomBanner
 import com.example.week6.Soal1.Components.FoodCard
 import com.example.week6.Soal1.Components.SearchBarVegetable
 import com.example.week6.Soal1.Components.TopNav
